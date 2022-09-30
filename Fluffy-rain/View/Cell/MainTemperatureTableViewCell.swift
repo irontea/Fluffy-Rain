d5fbe0ab4597b35debe34001c2c57f16c9def088
Artur <piro10@yandex.ru>
2022-09-30 14:35:50 +0300

    Добавлен UI для поиска города

--- Fluffy-rain/View/Cell/MainTemperatureTableViewCell.swift
+++ Fluffy-rain/View/Cell/MainTemperatureTableViewCell.swift
@@ -13,20 +13,8 @@ import SnapKit
 class MainTemperatureTableViewCell: UITableViewCell {
     
     static let identifier = "MainTemperatureCell"
-    let disposeBag = DisposeBag()
-    private var textFieldForCity: UITextField = {
-        let tf = UITextField()
-        tf.borderStyle = UITextField.BorderStyle.line
-        tf.textColor = .black
-        tf.font = UIFont.systemFont(ofSize: 28, weight: .bold)
-        return tf
-    }()
-    private var acceptButton: UIButton = {
-        let bt = UIButton()
-        bt.backgroundColor = .red
-        bt.setTitle("Accept", for: .normal)
-        return bt
-    }()
+    private let disposeBag = DisposeBag()
+
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -36,34 +24,16 @@ class MainTemperatureTableViewCell: UITableViewCell {
         fatalError()
     }
     
-    func setupUI() {
-        contentView.addSubview(textFieldForCity)
-        contentView.addSubview(acceptButton)
-        textFieldForCity.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(50)
-            make.leading.equalToSuperview()
-            
-            make.bottom.equalToSuperview()
-        }
-        acceptButton.snp.makeConstraints{ make in
-            make.top.equalToSuperview().offset(50)
-            make.leading.equalTo(textFieldForCity.snp.trailing)
-            make.trailing.equalToSuperview()
-            make.bottom.equalToSuperview()
-        }
+    private func setupUI() {
+
     }
     
     func configure(viewModel: MainViewModelProtocol) {
-        textFieldForCity.rx.text
-            .orEmpty
-            .bind(to: viewModel.city)
-            .disposed(by: disposeBag)
-        
-        acceptButton.rx.tap
-            .bind {
-                viewModel.fetchWeather()
-            }
-            .disposed(by: disposeBag)
+
+    }
+    
+    @objc func ch(){
+        print("Moscow")
     }
     
 }
--- Fluffy-rain/View/MainViewController.swift
+++ Fluffy-rain/View/MainViewController.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
-
+import SnapKit
+import RxCocoa
+import RxSwift
 
 class MainViewController: UIViewController {
     
     private var viewModel: MainViewModelProtocol!
     private let locationManager = LocationManager()
-    
-    private var mainTableView : UITableView = {
+//    private let city = ""
+    private let disposeBag = DisposeBag()
+    private let mainTableView : UITableView = {
         let tableView = UITableView()
         tableView.register(MainTemperatureTableViewCell.self, forCellReuseIdentifier: MainTemperatureTableViewCell.identifier)
         tableView.register(SevenDaysTableViewCell.self, forCellReuseIdentifier: SevenDaysTableViewCell.identifier)
@@ -21,21 +24,42 @@ class MainViewController: UIViewController {
         tableView.separatorStyle = .none
         return tableView
     }()
+    private let textFieldForCity: UITextField = {
+        let tf = UITextField()
+        tf.borderStyle = UITextField.BorderStyle.line
+        tf.textColor = .black
+        tf.font = UIFont.systemFont(ofSize: 28, weight: .bold)
+        tf.backgroundColor = .white
+        return tf
+    }()
+    private let acceptButton: UIButton = {
+        let bt = UIButton()
+        bt.backgroundColor = .red
+        bt.setTitle("Accept", for: .normal)
+        return bt
+    }()
+    private let backgroundView: UIView = {
+        let backgroundView = UIView()
+        return backgroundView
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel = MainViewModel(locationManager: locationManager)
         mainTableView.delegate = self
         mainTableView.dataSource = self
-        setupUI()
+        setupTableViewUI()
         setupNavigationBar()
+        setupSearchCityUI()
+        setupBinding()
         viewModel.setupViewModel()
     }
     override func viewWillAppear(_ animated: Bool) {
         self.mainTableView.tableFooterView = UIView(frame: CGRect.zero)
     }
     
-    func setupUI() {
+    private func setupTableViewUI() {
         view.addSubview(mainTableView)
         mainTableView.snp.makeConstraints { make in
             make.leading.equalToSuperview()
@@ -45,21 +69,67 @@ class MainViewController: UIViewController {
         }
     }
     
-    func setupNavigationBar() {
-        let appearence = UINavigationBarAppearance()
+   private func setupNavigationBar() {
+        let appearance = UINavigationBarAppearance()
         let switchTimeButton = UIBarButtonItem(image: UIImage(systemName: "sun.max"), style: .plain, target: self, action: #selector(mockSearch))
         let searchButton = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(mockSearch))
         
-        appearence.configureWithDefaultBackground()
-        appearence.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
-        navigationController?.navigationBar.tintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        navigationItem.scrollEdgeAppearance = appearence
-        navigationItem.standardAppearance = appearence
+        appearance.configureWithDefaultBackground()
+        appearance.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+        appearance.titleTextAttributes = [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), .font: UIFont.systemFont(ofSize: 18, weight: .semibold)]
+        self.navigationController?.navigationBar.tintColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        self.navigationItem.scrollEdgeAppearance = appearance
+        self.navigationItem.standardAppearance = appearance
         self.navigationItem.leftBarButtonItem = switchTimeButton
         self.navigationItem.rightBarButtonItem = searchButton
+//        self.navigationItem.title = viewModel.city.value ?? ""
+    }
+    
+    private func setupSearchCityUI(){
+        let safeArea = view.layoutMarginsGuide
+
+        view.addSubview(backgroundView)
+        backgroundView.addSubview(textFieldForCity)
+        backgroundView.addSubview(acceptButton)
+        
+        backgroundView.snp.makeConstraints { make in
+            make.top.equalTo(safeArea.snp.top)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.height.equalTo(30)
+        }
+        textFieldForCity.snp.makeConstraints { make in
+            make.top.equalTo(backgroundView.snp.top)
+            make.leading.equalTo(backgroundView.snp.leading)
+            make.height.equalTo(25)
+            make.width.equalTo(backgroundView.snp.width).multipliedBy(0.75)
+        }
+        acceptButton.snp.makeConstraints{ make in
+            make.top.equalTo(backgroundView.snp.top)
+            make.leading.equalTo(textFieldForCity.snp.trailing)
+            make.trailing.equalTo(backgroundView.snp.trailing)
+            make.height.equalTo(25)
+            make.width.equalTo(backgroundView.snp.width).multipliedBy(0.25)
+            
+        }
+    }
+    
+    private func setupBinding(){
+        textFieldForCity.rx.text
+            .orEmpty
+            .bind(to: viewModel.city)
+            .disposed(by: disposeBag)
+        
+        acceptButton.rx.tap
+            .bind {[weak self] in
+                guard let self = self else {return}
+                self.viewModel.fetchWeather()
+            }
+            .disposed(by: disposeBag)
     }
     
     @objc func mockSearch(){}
+    
 }
 
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
