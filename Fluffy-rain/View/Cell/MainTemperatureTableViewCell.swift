35c5d79f4968d652fa59e9a0f59aa8b8b6aaae21
Artur <piro10@yandex.ru>
2022-10-04 17:31:14 +0300

    Добавь констрейнты для MainTemperatureTableViewCell

--- Fluffy-rain/View/Cell/MainTemperatureTableViewCell.swift
+++ Fluffy-rain/View/Cell/MainTemperatureTableViewCell.swift
@@ -14,7 +14,23 @@ class MainTemperatureTableViewCell: UITableViewCell {
     
     static let identifier = "MainTemperatureCell"
     private let disposeBag = DisposeBag()
-
+    private let cityLable: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 37)
+        label.textAlignment = .center
+        return label
+    }()
+    private let weatherDescription: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.textAlignment = .left
+        return label
+    }()
+    private let mockView: UIView = {
+        let mockView = UIView()
+        mockView.backgroundColor = .black
+        return mockView
+    }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -25,15 +41,30 @@ class MainTemperatureTableViewCell: UITableViewCell {
     }
     
     private func setupUI() {
-
+        let safeArea = layoutMarginsGuide
+        
+        addSubview(mockView)
+        addSubview(weatherDescription)
+        addSubview(cityLable)
+        
+        cityLable.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(safeArea.snp.top).offset(14)
+        }
+        mockView.snp.makeConstraints { make in
+            make.top.equalTo(cityLable.snp.bottom).offset(18)
+            make.centerX.equalToSuperview()
+        }
+        weatherDescription.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(5)
+            make.top.equalTo(mockView).offset(37)
+        }
     }
     
     func configure(viewModel: MainViewModelProtocol) {
-
+        
     }
     
-    @objc func ch(){
-        print("Moscow")
-    }
+    
     
 }
